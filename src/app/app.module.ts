ea3dc8e9503fa8c587aa9c7eed971e9d3234dbd1
Johannes Bertens <johannes.bertens@gmail.com>
2017-03-17 18:25:19 +0100

    Improved navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
 
@@ -11,14 +14,28 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 import { ProfileService } from './services/profile.service';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MaterialCardsComponent } from './pages/material-cards/material-cards.component';
+
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'mat-cards', component: MaterialCardsComponent},
+  { path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    DashboardComponent,
+    MaterialCardsComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule,
